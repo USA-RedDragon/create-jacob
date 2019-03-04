04c023fa743e9e9df3da57ef585bb66fca541217
Jacob McSwain <jacob@clevyr.com>
2019-03-04 12:22:44 -0600

    Make it do the thing

--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
-console.log('Hello World');
+import { readFileSync } from 'fs';
+import path from 'path';
+
+import { ArgumentParser } from 'argparse';
+import inquirer from 'inquirer';
+
+import allSettings from './wizard/settings';
+
+const version = JSON.parse(readFileSync(path.join(__dirname, '../package.json'))).version;
+
+const parser = new ArgumentParser({
+    version: version,
+    addHelp: true,
+    description: 'Creates an API backend',
+});
+
+parser.addArgument(['projectName'], { help: 'The name of the project to make' });
+const args = parser.parseArgs();
+
+async function main() {
+    const promptResult = await inquirer.prompt(allSettings);
+
+    console.log(promptResult);
+    console.log(args);
+}
+
+main();
